e765f9b3709a0f1087073d7c4008384ddffe21dd
dimitarkocevski47 <dimitarkocevski47@gmail.com>
2026-06-10 20:45:50 +0200

    Demo code added

---
+++ Classes/Class 13/TaxiManager9000/TaxiManager9000.Domain/Models/User.cs
@@ -0,0 +1,26 @@
+namespace TaxiManager9000.Domain.Models;
+
+using TaxiManager9000.Domain.BaseEntity;
+using TaxiManager9000.Domain.Enums;
+
+public class User : BaseEntity
+{
+
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public Role Role { get; set; }
+
+    public User(string username, string password, Role role)
+    {
+        Username = username;
+        Password = password;
+        Role = role;
+    }
+
+    public User() { }
+
+    public override string GetInfo()
+    {
+        return $"User with username: [{Username}] and role {Role}";
+    }
+}
